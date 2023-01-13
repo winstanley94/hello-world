9bd0c6aa8d57a366755d5e0923c93142bef1209b
winstanley94 <122543461+winstanley94@users.noreply.github.com>
2023-01-13 09:36:12 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Happy)
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.No)
+})
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Heart)
 })
